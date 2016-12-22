13e89258d31307b061d0ec6a4fbe2d95cf3fad2f
KerwinKoo <gukaiqiang@gmail.com>
2016-12-22 18:10:52 +0800

    add login static test file

--- main.go
+++ main.go
@@ -12,9 +12,13 @@ func main() {
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "time=${time_rfc3339}, method=${method}, uri=${uri}, status=${status}\n",
 	}))
-	e.Use(middleware.AddTrailingSlash())
-	e.GET("/wifidog/ping/", wdmethods.Ping)
-	e.GET("/wifidog/login/", wdmethods.Login)
+
+	wdGroup := e.Group("/wifidog")
+	// wdGroup.Use(middleware.AddTrailingSlash())
+	wdGroup.Static("/login/", "static/login.html")
+	wdGroup.GET("/ping/", wdmethods.Ping)
+
+	// wdGroup.GET("login/", wdmethods.Login)
 
 	e.Logger.Fatal(e.Start(":8082"))
 }

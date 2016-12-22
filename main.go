17530c0bcb777b635acf989a5b85a0efd748914e
KerwinKoo <gukaiqiang@gmail.com>
2016-12-22 14:22:36 +0800

    add log middleware to show wifidog request URI

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"github.com/KerwinKoo/gowauth/wdmethods"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "time=${time_rfc3339}, method=${method}, uri=${uri}, status=${status}\n",
+	}))
 	e.GET("/wifidog/ping/", wdmethods.Ping)
 	e.GET("/wifidog/login/", wdmethods.Login)
 

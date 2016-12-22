aab379c36ec7623c72775d7a228ac96967f72618
KerwinKoo <gukaiqiang@gmail.com>
2016-12-22 17:28:10 +0800

    add an login html example

--- main.go
+++ main.go
@@ -8,9 +8,11 @@ import (
 
 func main() {
 	e := echo.New()
+	e.Static("/static", "static")
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "time=${time_rfc3339}, method=${method}, uri=${uri}, status=${status}\n",
 	}))
+	e.Use(middleware.AddTrailingSlash())
 	e.GET("/wifidog/ping/", wdmethods.Ping)
 	e.GET("/wifidog/login/", wdmethods.Login)
 
